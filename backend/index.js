837d8cc304a99a930e8c6d3651b8904d0cdf2ada
Zory <zoryana.tischenko@gmail.com>
2015-10-20 11:53:21 +0200

    edited as per tutorial

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,18 @@ app.use(function (req, res, next) {
     next();
 });
 
+var sendResponse = function (res, error, message) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(message);
+    }
+    return res.json({
+        error: error && error.message || error,
+        message: message
+    });
+};
+
 /**
  * Init params from environment variables. Innometrics platform sets environment variables during installation
  * to your Cloud Platform of choice.
@@ -36,26 +48,98 @@ var innoHelper = new inno.InnoHelper(vars);
 // POST request to "/" is always expected to recieve stream with events
 app.post('/', function (req, res) {
 
-    var profile = innoHelper.getProfileFromRequest(req.body);
-    var session = profile.getLastSession();
-    var events  = session.getEvents();
-    var event   = events[0];
-    var url     = event.getDataValue('url');
+    try {
+        var profile = innoHelper.getProfileFromRequest(req.body);
+        var session = profile.getLastSession();
+        var events  = session.getEvents();
+        var event   = events[0];
+        var url     = event.getDataValue('url');
+        if (!url) {
+            throw Error('URL not found');
+        }
+    } catch (err) {
+        return sendResponse(res, err);
+    }
+ 
     
-    innoHelper.getAppSettings(function (err, settings) {
-        console.log(settings.api_key);
-    });
 
-    return res.json(event);
-});
-
-innoHelper.getAppSettings(function (err, settings) {       
+innoHelper.getAppSettings(function (err, settings) {   
+    if (err) {
+            return sendResponse(res, err);
+        }
+    
     var alchemyUrl = 'http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?apikey=' + settings.api_key + '&url=' + url + '&outputMode=json';
-    request.get(alchemyUrl, function (err, res) {
-        var result = JSON.parse(res.body);
-    });      
+    
+    request.get(alchemyUrl, function (err, res) {
+        
+        if (err) {
+            return sendResponse(res, err);
+        }
+        
+            var result = JSON.parse(res.body);
+            if (result && result.status === 'ERROR') {
+                return sendResponse(res, result.statusInfo);
+            }
+
+        
+            var interests = getInterests(result.entities, settings);
+
+            if (!interests.length) {
+                return sendResponse(res, null, 'No attributes to update');
+            }
+                inno.loadProfile(profile.getId(), function (err, fullProfile) {
+                
+                if (err) {
+                    return sendResponse(res, err);
+                }
+
+                try {
+                    interests.forEach(function (item) {
+                        var id = getAttributeId(item.text);
+                        var count = parseInt(item.count, 10);
+                        var attribute = fullProfile.getAttribute(id, innoHelper.getCollectApp(), settings.section);
+
+                        if (!attribute) {
+                            attribute = new inno.Profile.Attribute({
+                                name: id,
+                                value: count,
+                                section: settings.section,
+                                collectApp: innoHelper.getCollectApp()
+                            });
+                            fullProfile.setAttribute(attribute);
+                        } else {
+                            var current = attribute.getValue();
+                            attribute.setValue((current + count) / 2);
+                        }
+                    });
+                } catch (err) {
+                    return sendResponse(res, err);
+                }
+                    
+                innoHelper.saveProfile(fullProfile, function (err) {
+                   if (err) {
+                        return sendResponse(res, err);
+                     }
+                    return sendResponse(res, err, "Profile was successfully updated");
+ 
+                    
+                });
+        });
+        
+    });     
 });
 
+ });
+
+var getAttributeId = function (name) {
+    return name.toLowerCase().replace(new RegExp(' +', 'g'), '-').replace(new RegExp('[^-a-z0-9]', 'g'), '');
+};
+
+var getInterests = function (entities, settings) {
+    return entities.filter(function (item) {
+        return (settings.entityType.indexOf(item.type) > -1) && (parseFloat(item.relevance) >= settings.minRelevance);
+    }).splice(0, settings.amount);
+};
 
 // Starting server
 var server = app.listen(port, function () {
